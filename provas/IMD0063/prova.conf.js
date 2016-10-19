391e25fadc602ccfb723b338c6705266606dc6a1
Paulo Sergio <paulosergios11@hotmail.com>
2016-10-19 12:42:12 -0300

    Letras descrevendo as alternativas

---
+++ provas/IMD0063/prova.conf.js
@@ -0,0 +1,49 @@
+var exam = 
+    {
+        "1":{
+            "numero": 1,
+            "objetiva": true,
+            "url": "questoes/1/",
+            "enunciado": "enunciado.jpg",
+            "alternativas": {
+                "a": "alternativa_a.jpg",
+                "b": "alternativa_b.jpg",
+                "c": "alternativa_c.jpg",
+                "d": "alternativa_d.jpg",
+                "e": "alternativa_e.jpg"
+            }
+        },
+
+        "2":{
+            "numero": 2,
+            "objetiva": true,
+            "url": "questoes/2/",
+            "enunciado": "enunciado.jpg",
+            "alternativas": {
+                "a": "alternativa_a.jpg",
+                "b": "alternativa_b.jpg",
+                "c": "alternativa_c.jpg",
+                "d": "alternativa_d.jpg",
+                "e": "alternativa_e.jpg"
+            }
+        },
+
+        "3":{
+            "numero": 3,
+            "objetiva": false,
+            "url": "questoes/3/",
+            "enunciado": "enunciado.jpg"
+        },
+        "4":{
+            "numero": 4,
+            "objetiva": false,
+            "url": "questoes/3/",
+            "enunciado": "enunciado.jpg"
+        },
+        "5":{
+            "numero": 5,
+            "objetiva": false,
+            "url": "questoes/3/",
+            "enunciado": "enunciado.jpg"
+    }
+}
